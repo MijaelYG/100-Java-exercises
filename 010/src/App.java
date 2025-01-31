ff387f23d034bddd04d2fb93e05ba15e0600096d
MijaelYG <yauyogranadosmijael@gmail.com>
2025-01-31 18:32:23 -0500

    cambios ejercicio 10

--- 010/src/App.java
+++ 010/src/App.java
@@ -9,8 +9,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         int[] numeros = { 10, 5, 1, 8, 9, 15 };
 
-        int numeroMayor=Integer.MIN_VALUE; // or numeros[0]
-        int numeroMenor=Integer.MAX_VALUE; // or numeros[0]
+        int numeroMayor=Integer.MIN_VALUE; // Mayor numero
+        int numeroMenor=Integer.MAX_VALUE; // Menor numero
 
         System.out.println("numeros: " + Arrays.toString(numeros));
         for (int i = 0; i < numeros.length; i++) {
